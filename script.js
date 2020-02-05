3fdf1701ebd77e5370ded8e450f79890d3d4cbd3
mekaala <mekaala@gmail.com>
2020-02-05 11:00:09 -0500

    developed JS format for Jeopardy questions

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// Selected for each individual button by query selecting for both their class and id. 
+// Selected for each individual button by query selecting for both their class and id.
+
+// To make the questions, I will need a prompt that asks a question
+// and an empty text box that asks for an answer.
+// If the input is the same as the answer variable, then the answer is correct,
+// and the accrued points will add to the total.
+// Else: the player gets no points, and will return to the screen.
 
 
 
@@ -9,7 +15,55 @@ const nin400 = document.querySelector(".Nintendo > #fourth");
 const nin500 = document.querySelector(".Nintendo > #fifth");
 
 
+nin100.addEventListener('click', nintendo100);
+function nintendo100() {
+    let quiz = prompt("Who is Mario's brother?") 
+    if (quiz.toUpperCase() == "LUIGI") {
+        alert("Good job! You have earned 100 points.")
+    } else {
+        alert("Really? You couldn't get the 100 point question right?")
+    }
+}
 
+nin200.addEventListener('click', nintendo200);
+function nintendo200() {
+    let quiz = prompt("Who is Mario's brother?") 
+    if (quiz.toUpperCase() == "LUIGI") {
+        alert("Good job! You have earned 100 points.")
+    } else {
+        alert("Really? You couldn't get the 100 point question right?")
+    }
+}
+
+nin300.addEventListener('click', nintendo300);
+function nintendo300() {
+    let quiz = prompt("Who is Mario's brother?") 
+    if (quiz.toUpperCase() == "LUIGI") {
+        alert("Good job! You have earned 100 points.")
+    } else {
+        alert("Really? You couldn't get the 100 point question right?")
+    }
+}
+
+nin400.addEventListener('click', nintendo400);
+function nintendo400() {
+    let quiz = prompt("Who is Mario's brother?") 
+    if (quiz.toUpperCase() == "LUIGI") {
+        alert("Good job! You have earned 100 points.")
+    } else {
+        alert("Really? You couldn't get the 100 point question right?")
+    }
+}
+
+nin500.addEventListener('click', nintendo500);
+function nintendo500() {
+    let quiz = prompt("Before publishing The Legend of Zelda: Breath of the Wild in 2017, what open world game did Nintendo publish in 2015?") 
+    if (quiz.toUpperCase() == "XENOBLADE CHRONICLES X" || quiz.toUpperCase() == "XENOBLADE X" || quiz.toUpperCase() == "XENOBLADEX") {
+        alert("NO WAY! You got it right! 500 points!")
+    } else {
+        alert("Yeah... I wouldn't blame you for getting this one wrong...")
+    }
+}
 
 
 const pho100 = document.querySelector(".Photography > #first");
