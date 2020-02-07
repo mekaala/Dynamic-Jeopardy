183e32beaa5cd9ee2bb38152a3c7ec0225c1561a
mekaala <mekaala@gmail.com>
2020-02-07 15:53:22 -0500

    added profile photo, updated about section to be a modal box

--- script.js
+++ script.js
@@ -434,3 +434,32 @@ function finalScore() {
 const gameOver = new Audio('sounds/Game-over effect.mp3');
 const niceWork = new Audio('sounds/nice_work.mp3');
 const incredible = new Audio('sounds/incredible.mp3');
+
+
+
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("aboutMe");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
